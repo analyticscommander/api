6d6f1a272b5e68fab09bb5a1b592aa5c96a896d2
Ruben <rubenm.6315@gmail.com>
2019-01-23 20:30:50 -0500

    message

---
+++ api.py
@@ -0,0 +1,10 @@
+# def get_teams_by_season():
+# def save_team_by_season():
+# def get_rosters_by_team():
+# def save_roster_by_team():
+# def get_players_by_roster():
+# def save_player_by_roster():
+def get_api_response(url):
+    print(url)
+def main():
+    teams = get_api_response(url)
